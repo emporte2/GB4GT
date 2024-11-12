53f3619a9a2c94bd83b27b54d01923044b6b89c1
Erica Porter <emporte@96.200.21.198.tigernet.wifi.dyn.clemson.edu>
2024-11-12 11:44:36 -0500

    Added function to compute gradients for training folds only

--- CV_functions.R
+++ CV_functions.R
@@ -1,4 +1,51 @@
 ###############################################################################
+# R function: this function Computes subject-specific gradients for a CV fold.
+# Input: 
+# p = length N vector of probabilities
+# C = matrix identifying subjects assigned to the kth masterpool, format first column is number of individuals, then followed by indices of individuals 
+# B = matrix identifying the test results associated with kth masterpool, first column is number of results, followed by indices of test results 
+# Y = matrix whose ith row is of the form (col1=0, col2=number of pools ith individual involved in, col3:col(3+col2-1)=pools ith individual was assigned to)
+# Z = a matrix of testing responses
+# Se = sensitivity
+# Sp = specificity
+# k.train = indices of subgroups (out of K) used for training
+
+grad.GT.train <- function(p,C,B,Y,Z,Se,Sp,k.train){
+  
+  N<-dim(Y)[1]  # number of (individual) observations
+  K<-dim(C)[1]  # number of non-overlapping subgroups for forming the likelihood
+  
+  dprob.Z<-rep(-99,N)
+  for(k in k.train){
+    
+    ind<-C[k,2:(C[k,1]+1)]  #Pull individuals associated with kth group
+    ptemp<-p[ind]           #Pull probs for individuals associated with kth group
+    cj<-length(ind)         # number of individuals in group k
+    tmat<-expand.grid(replicate(cj, 0:1, simplify = FALSE))  # all 2^cj possible combinations of 0's and 1's 
+    prod.pi<-apply(t(t(tmat)*ptemp) + t(t(abs(1-tmat))*(1-ptemp)),1,prod)  # gets a probability for each combinations of 0's and 1's for th kth group
+    
+    SeSp<-1
+    # Zd for test outcome, Zt for true outcome
+    for(t in 1:B[k,1]){
+      Zd<-Z[B[k,(t+1)],1]  # test outcomes for kth group
+      Yid<-Z[B[k,(t+1)],4:(Z[B[k,(t+1)],2]+3)]  # individuals contributing to test outcome
+      id<-match(Yid, ind)
+      Zt<-apply(as.matrix(tmat[,id]),1,sum)>0  # vector of all true statuses of Zd 
+      SeSp<- (Zt*Zd*Se + Zt*(1-Zd)*(1-Se) + (1-Zt)*Zd*(1-Sp) +(1-Zt)*(1-Zd)*Sp)*SeSp
+    }
+    
+    prob.Z<-sum(SeSp*prod.pi)
+    
+    for(i in ind){
+      id<-match(i, ind)
+      term<-(1-p[i])*tmat[,id] -p[i]*(1-tmat[,id])
+      dprob.Z[i]<-sum(SeSp*prod.pi*term)/prob.Z
+    }
+  }
+  return(dprob.Z)
+}
+
+###############################################################################
 # R function: this function performs cross-validation for gradient boosting
 #             for group testing data using regression trees as the weak learner.
 # Input: 
