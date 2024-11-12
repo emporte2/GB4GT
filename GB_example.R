d9ce64132700876f458884af136356956c874aca
Erica Porter <emporte@96.200.21.198.tigernet.wifi.dyn.clemson.edu>
2024-11-12 14:14:43 -0500

    Added script for final example.

---
+++ GB_example.R
@@ -0,0 +1,194 @@
+###############################################################################
+# Read in necessary functions 
+# Set R directory to your location that conatins the GB4GT R scripts.
+
+source("GB_functions.R")
+library(rpart)
+library(splines)
+
+# for illustration, we will specify tuning parameter values for the weak 
+# learners in this example.  To obtain complete cross-validation results, you may 
+# use the functions in CV_functions.R to select your desired tuning parameters.
+# source("CV_functions.R")
+
+###############################################################################
+# Generate group testing data set according to Dorfman testing protocol.
+
+set.seed(449)
+
+N <- 10000  # sample size
+cj <- 4     # pool size
+Se <- 0.95  # sensitivity
+Sp <- 0.98  # specificity
+
+X <- cbind(rep(1,N),sort(runif(N,0,2*pi))) # design matrix
+p.true <- 1/(1+exp(-(-2+0.5*sin(X[,2]))))  # true infection probabilities
+Y.true <- rbinom(N,1,p.true)               # true infection statuses
+
+# Simulate group testing (Dorfman testing)
+test.res <- Dorfman.decode.same.error(Y.true,Se,Sp,cj)
+
+Z <- test.res$Z # A matrix of testing responses whose jth row is of the form 
+#               (col1=Z_j, col2=cj, col3=assay used, col4:col(4+cj-1)=indices 
+#               of the individuals assigned to the jth pool) 
+
+Y <- test.res$Y # matrix whose ith row is of the form (col1=0, col2=number of 
+#                 pools ith individual involved in, col3:col(3+col2-1)=pools ith
+#                 individual was assigned to)
+
+# Build the C matrix for tracking subjects assigned to the kth masterpool
+K<-N/cj
+C<-matrix(NA, nrow=K, ncol=cj+1)
+for(k in 1:K){
+  C[k,]<-c(cj, ((k-1)*cj+1):(k*cj))
+}
+
+# Build the B matrix for tracking tests associated with kth masterpool
+
+# Note using 500 here to make sure we have enough room, it is trimmed off below
+B<-matrix(NA,nrow= K, ncol=500)  
+
+for(k in 1:K){
+  
+  mps<-C[k,1]
+  ind<-C[k,2:(mps+1)]
+  temp<-NULL
+  
+  for(i in ind){
+    np<-Y[i,2]
+    temp<-c(temp,Y[i,3:(np+2)])
+  }
+  temp<-unique(temp)
+  nt<-length(temp)
+  B[k,1]<-nt
+  B[k,2:(nt+1)]<-temp
+}
+B<-B[,apply(is.na(B),2,mean)<1]   # Trimmed here
+
+Yd<-rep(-99,N)
+for(i in 1:dim(Y)[1]){
+  tests<-Y[i,3:(Y[i,2]+2)]  # identifies the pool(s) that individual i is in
+  Yd[i]<-prod(Z[tests,1])   # multiplies observed (group) test outcomes that individual i was involved in
+}                           # Yd indicates if an individual ever tested positive
+
+################################################################################
+# Perform gradient boosting to obtain estimated infection probabilities for
+# all N individuals using *regression trees* as the weak learner
+
+# initialize values for gradient boosting
+phat <- mean(Yd)
+F0 <- rep(log(phat/(1-phat)),N)
+p0 <- exp(F0)/(1+exp(F0))
+
+epoch <- 10
+maxdepth <- 1
+minbucket <- 300
+cp <- 1e-16
+
+for(i in 1:epoch){
+  
+  SR <- grad.GT(p0,C,B,Y,Z,Se,Sp)
+  
+  train <- cbind(SR, X[,-1])
+  colnames(train) <- c("Z","X")
+  train <- as.data.frame(train)
+  
+  fit <- rpart(Z~X, data=train, method  = "anova", 
+               control = list(maxdepth = maxdepth,
+                              minbucket = minbucket,
+                              cp=cp))
+  h <-  predict(fit, newdata=train)
+  
+  lam <- optimize(loglik.optim,interval=c(-10,10), F0=F0, h=h, C=C, B=B, N=N, k.ind=1:K, obs.ind=1:N)$minimum
+  
+  F0 <- F0 + lam*h
+  
+  p0 <- exp(F0)/(1+exp(F0))
+}
+
+F0.trees <- F0
+
+################################################################################
+# Perform gradient boosting to obtain estimated infection probabilities for
+# all N individuals using *kernel smoothing* as the weak learner
+
+# initialize values for gradient boosting
+phat <- mean(Yd)
+F0 <- rep(log(phat/(1-phat)),N)
+p0 <- exp(F0)/(1+exp(F0))
+
+epoch <- 5
+bandwidth <- 2
+
+for(i in 1:epoch){
+  
+  SR <- grad.GT(p0,C,B,Y,Z,Se,Sp)
+  
+  train <- cbind(SR, X[,-1])
+  colnames(train) <- c("SR","X")
+  train <- as.data.frame(train)
+  
+  h <- ksmooth(train$X, train$SR, kernel="normal", bandwidth=bandwidth)$y  
+
+  lam <- optimize(loglik.optim,interval=c(-10,10), F0=F0, h=h, C=C, B=B, N=N, k.ind=1:K, obs.ind=1:N)$minimum
+  
+  F0 <- F0 + lam*h
+  
+  p0 <- exp(F0)/(1+exp(F0))
+}
+
+F0.ks <- F0
+
+################################################################################
+# Perform gradient boosting to obtain estimated infection probabilities for
+# all N individuals using *splines* as the weak learner
+
+# initialize values for gradient boosting
+phat <- mean(Yd)
+F0 <- rep(log(phat/(1-phat)),N)
+p0 <- exp(F0)/(1+exp(F0))
+
+epoch <- 2
+nint <- 1
+
+for(i in 1:epoch){
+  
+  SR <- grad.GT(p0,C,B,Y,Z,Se,Sp)
+  
+  train <- cbind(SR, X[,-1])
+  colnames(train) <- c("SR","X")
+  train <- as.data.frame(train)
+  
+  knots <- quantile(train$X, prob=seq(0,1,length.out=(nint+2))[-c(1,nint+2)])
+  Btrain <- bs(train$X, knots=knots)
+  
+  fit <- lm(train$SR ~ Btrain, data=train)
+  
+  h <- cbind(1,Btrain)%*%coef(fit)
+  
+  lam <- optimize(loglik.optim,interval=c(-10,10), F0=F0, h=h, C=C, B=B, N=N, k.ind=1:K, obs.ind=1:N)$minimum
+  
+  F0 <- F0 + lam*h
+  
+  p0 <- exp(F0)/(1+exp(F0))
+}
+
+F0.splines <- F0
+
+###############################################################################
+# plot results
+
+par(mfrow=c(1,3))
+plot(X[,2], 0.5*sin(X[,2])-2, col="azure3", ylim=c(-2.8,-1.4),
+     xlab="x", ylab="F(x)", main="GB with \n regression trees")
+lines(X[,2], F0.trees, col="black")
+
+plot(X[,2], 0.5*sin(X[,2])-2, col="azure3", ylim=c(-2.8,-1.4),
+     xlab="x", ylab="F(x)", main="GB with \n kernel smoothing")
+lines(X[,2], F0.ks, col="black")
+
+plot(X[,2], 0.5*sin(X[,2])-2, col="azure3", ylim=c(-2.8,-1.4),
+     xlab="x", ylab="F(x)", main="GB with \n splines")
+lines(X[,2], F0.splines, col="black")
+
+
---
+++ GB_functions.R
@@ -0,0 +1,145 @@
+###############################################################################
+# R function: this function computes subject-specific gradients for all observations
+# Input: 
+# p = length N vector of probabilities
+# C = matrix identifying subjects assigned to the kth masterpool, format first column is number of individuals, then followed by indices of individuals 
+# B = matrix identifying the test results associated with kth masterpool, first column is number of results, followed by indices of test results 
+# Y = matrix whose ith row is of the form (col1=0, col2=number of pools ith individual involved in, col3:col(3+col2-1)=pools ith individual was assigned to)
+# Z = a matrix of testing responses
+# Se = sensitivity
+# Sp = specificity
+
+grad.GT<-function(p,C,B,Y,Z,Se,Sp){
+  
+  N<-dim(Y)[1]  # number of (individual) observations
+  K<-dim(C)[1]  # number of non-overlapping subgroups for forming the likelihood
+  
+  dprob.Z<-rep(-99,N)
+  for(k in 1:K){
+    
+    ind<-C[k,2:(C[k,1]+1)]  #Pull individuals associated with kth group
+    ptemp<-p[ind]           #Pull probs for individuals associated with kth group
+    cj<-length(ind)         # number of individuals in group k
+    tmat<-expand.grid(replicate(cj, 0:1, simplify = FALSE))  # all 2^cj possible combinations of 0's and 1's 
+    prod.pi<-apply(t(t(tmat)*ptemp) + t(t(abs(1-tmat))*(1-ptemp)),1,prod)  # gets a probability for each combinations of 0's and 1's for th kth group
+    
+    SeSp<-1
+    # Zd for test outcome, Zt for true outcome
+    for(t in 1:B[k,1]){
+      Zd<-Z[B[k,(t+1)],1]  # test outcomes for kth group
+      Yid<-Z[B[k,(t+1)],4:(Z[B[k,(t+1)],2]+3)]  # individuals contributing to test outcome
+      id<-match(Yid, ind)
+      Zt<-apply(as.matrix(tmat[,id]),1,sum)>0  # vector of all true statuses of Zd 
+      SeSp<- (Zt*Zd*Se + Zt*(1-Zd)*(1-Se) + (1-Zt)*Zd*(1-Sp) +(1-Zt)*(1-Zd)*Sp)*SeSp
+    }
+    
+    prob.Z<-sum(SeSp*prod.pi)
+    
+    for(i in ind){
+      id<-match(i, ind)
+      term<-(1-p[i])*tmat[,id] -p[i]*(1-tmat[,id])
+      dprob.Z[i]<-sum(SeSp*prod.pi*term)/prob.Z
+    }
+  }
+  return(dprob.Z)
+}
+
+###############################################################################
+# R function: this function is used to select the
+#             learning rate at each epoch/iteration by optimizing the
+#             log-likelihood given the current model fit.
+# Input: 
+# lam = learning rate
+# F0 = current model fit
+# h = current weak learner fit
+# C = matrix identifying subjects assigned to the kth masterpool, format first column is number of individuals, then followed by indices of individuals 
+# B = matrix identifying the test results associated with kth masterpool, first column is number of results, followed by indices of test results 
+# N = total number of individuals
+# k.ind = indices of subgroups used to calculate likelihood (set to 1:K for entire data set)
+# obs.ind = indices of individuals used to calculate likelihood (set to 1:N for entire data set)
+
+loglik.optim <- function(lam,F0,h,C,B,N,k.ind,obs.ind){
+  
+  F <- F0+lam*h
+  
+  p.sub <- exp(F)/(1+exp(F))
+  p <- rep(NA,N)
+  p[obs.ind] <- p.sub
+  
+  prob.Z <- rep(NA,length(k.ind))
+  for(k in k.ind){
+    
+    ind <- C[k,2:(C[k,1]+1)]  #Pull individuals associated with kth group
+    ptemp <- p[ind]           #Pull probs for individuals associated with kth group
+    cj <- length(ind)         # number of individuals in group k
+    tmat <- expand.grid(replicate(cj, 0:1, simplify = FALSE))  # all 2^cj possible combinations of 0's and 1's 
+    prod.pi <- apply(t(t(tmat)*ptemp) + t(t(abs(1-tmat))*(1-ptemp)),1,prod)  # gets a probability for each combinations of 0's and 1's for th kth group
+    
+    SeSp <- 1
+    # Zd for test outcome, Zt for true outcome
+    for(t in 1:B[k,1]){
+      Zd <- Z[B[k,(t+1)],1]  # test outcomes for kth group
+      Yid <- Z[B[k,(t+1)],4:(Z[B[k,(t+1)],2]+3)]  # individuals contributing to test outcome
+      id <- match(Yid, ind)
+      Zt <- apply(as.matrix(tmat[,id]),1,sum)>0  # vector of all true statuses of Zd 
+      SeSp <- (Zt*Zd*Se + Zt*(1-Zd)*(1-Se) + (1-Zt)*Zd*(1-Sp) +(1-Zt)*(1-Zd)*Sp)*SeSp
+    }
+    
+    prob.Z[k] <- sum(SeSp*prod.pi)
+  }
+  
+  loglik <- -sum(log(na.omit(prob.Z)))
+  
+  return(loglik=loglik)
+}
+
+###############################################################################
+# R function: This function simulates Dorfman decoding and stores the 
+#             testing responses in accordance to the data structure 
+#             required to perform gradient boosting for group testing
+#             data as described in Porter et al. (202X)
+#
+# Input: 
+# Y.true = The true statuses of the individuals
+# Se = The testing sensitivity used for both pools and individual testing
+# Sp = The testing specificity used for both pools and individual testing
+# cj = pool size to be used
+
+Dorfman.decode.same.error<-function(Y.true,Se,Sp,cj){
+  N<-length(Y.true)
+  Jmax<-N+N/cj 
+  J<-1
+  
+  Y<-matrix(-99,nrow=N, ncol=4) 
+  Z<-matrix(-99,nrow=Jmax,ncol=cj+3) 
+  
+  
+  for(j in 1:(N/cj)){
+    prob<-ifelse(sum(Y.true[((j-1)*cj+1):(j*cj)])>0,Se,1-Sp)
+    Z[J,1]<-rbinom(n=1,size=1,prob=prob)
+    Z[J,2]<-cj
+    Z[J,3]<-1
+    Z[J,4:(cj+3)]<-((j-1)*cj+1):(j*cj)
+    Y[((j-1)*cj+1):(j*cj),2]<-1
+    Y[((j-1)*cj+1):(j*cj),3]<-J
+    J<-J+1
+    if(Z[J-1,1]==1){
+      for(k in ((j-1)*cj+1):(j*cj)){
+        prob<-ifelse(Y.true[k]>0,Se,1-Sp)
+        Z[J,1]<- rbinom(n=1,size=1,prob=prob)
+        Z[J,2]<-1
+        Z[J,3]<-1
+        Z[J,4]<-k
+        Y[k,2]<-2
+        Y[k,4]<-J
+        J<-J+1
+      }
+    }
+  }
+  
+  J<-J-1
+  Z<-Z[1:J,]
+  
+  return(list("Z"=Z, "Y"=Y))
+}
+
